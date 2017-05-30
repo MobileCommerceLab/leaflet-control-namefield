d503c756bdc36f53ab67fd155fb096ff4e8e8f0a
Daniel Smullen <dsmullen@outbreaklabs.com>
2017-05-30 18:07:37 -0400

    updated build, adding dist to repo
    build script no longer generates tests, since we don't use that
    infrastructure anyway

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,5 +4,3 @@ mkdir -p dist
 
 browserify src/index.js -t browserify-shim -t es3ify --standalone leaflet-control-geocoder | derequire >dist/Control.Geocoder.js
 cp -a Control.Geocoder.css images/ dist/
-
-browserify test/browserify.js -o test/bundle.js
