233d8a75a9bcad6aa464cf079e2558297efbc193
Daniel Smullen <dsmullen@outbreaklabs.com>
2017-05-31 16:33:36 -0400

    Update nominatim.js
    provider now returns a leaflet geoJSON object rather than the raw object

--- src/geocoders/nominatim.js
+++ src/geocoders/nominatim.js
@@ -45,6 +45,7 @@ module.exports = {
 				var results = [];
 				for (var i = data.length - 1; i >= 0; i--) {
 					var bbox = data[i].boundingbox;
+					var geojson_data = data[i].geojson;
 					for (var j = 0; j < 4; j++) bbox[j] = parseFloat(bbox[j]);
 					results[i] = {
 						icon: data[i].icon,
@@ -55,7 +56,7 @@ module.exports = {
 							: undefined,
 						bbox: L.latLngBounds([bbox[0], bbox[2]], [bbox[1], bbox[3]]),
 						center: L.latLng(data[i].lat, data[i].lon),
-						geojson: data[i].geojson,
+						geojson: L.geoJSON(geojson_data),
 						properties: data[i]
 					};
 				}
