c3bd4b08cbba0cdb2939cc51ec533bd823dc79d4
Daniel Smullen <dsmullen@outbreaklabs.com>
2017-06-20 15:29:08 -0400

    v

--- dist/Control.NameSelector.js
+++ dist/Control.NameSelector.js
@@ -29,23 +29,37 @@ module.exports = {
     onAdd: function (map) {
       var className = 'leaflet-control-geocoder-nameselector',
           container = L.DomUtil.create('div', className + ' leaflet-bar'),
+          icon = L.DomUtil.create('button', className + '-icon', container),
           form = this._form = L.DomUtil.create('div', className + '-form', container);
 
       this._map = map;
       this._container = container;
+
+      icon.innerHTML = '&nbsp;';
+      icon.type = 'button';
 
       this._inputField = L.DomUtil.create('input', '', form);
       this._inputField.type = 'text';
       this._inputField.placeholder = this.options.placeholder;
 
       L.DomEvent.addListener(this._inputField, 'keydown', this._keydown, this);
+      L.DomEvent.addListener(icon, 'mousedown', this._doNotify, this);
 
       L.DomEvent.disableClickPropagation(container);
 
       return container;
     },
 
-    _setName: function(result) {
+    getField: function(){
+      return this._inputField.value;
+    },
+
+    setField: function(arg){
+      this._inputField.value = arg;
+      this._doNotify();
+    },
+
+    _setField: function(result) {
       result = result.geocode || result;
       this._inputField.value = result.name;
       this._doNotify();
@@ -54,7 +68,7 @@ module.exports = {
     },
 
     _doNotify: function () {
-      this.fire('name-changed', {name: this._inputField.value});
+      this.fire('field-changed', {name: this._inputField.value});
     },
 
     _keydown: function(e) {
