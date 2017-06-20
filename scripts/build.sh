f1b68f6cb68bef50b16fcaf3c828cb3eac3f98f7
Daniel Smullen <dsmullen@outbreaklabs.com>
2017-06-20 09:45:15 -0400

    repo cleanup

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,5 +2,5 @@
 
 mkdir -p dist
 
-browserify src/index.js -t browserify-shim -t es3ify --standalone leaflet-control-geocoder | derequire >dist/Control.Geocoder.js
-cp -a Control.Geocoder.css images/ dist/
+browserify src/index.js -t browserify-shim -t es3ify --standalone leaflet-control-namefield | derequire >dist/Control.NameSelector.js
+cp -a Control.NameSelector.css images/ dist/
